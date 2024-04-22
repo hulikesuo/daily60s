666828f908955a8bc3453011d30f386c3d368093
hulikesuo <167519414+hulikesuo@users.noreply.github.com>
2024-04-22 13:59:36 +0800

    Update main.py
    只保留文本

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import os
 import requests
+import json
 
 pushplus_token = os.environ.get('pushplus_token')
 topic = os.environ.get('topic')
 
-image_url = "https://60s.viki.moe/bing?e=image"
-
-text_url = "https://60s.viki.moe/bing?e=text"
+text_url = "https://60s.viki.moe/60s?v2=1"
 
 text_response = requests.get(text_url)
 content = text_response.text
+content = json.loads(content)
+s = content['data']['news']
+m = content['data']['tip']
+c = ''
+for i in range(len(s)):
+    c += f'{i+1}、{s[i]}\n'
+c += f'微语: {m}'
 
 pushplus_url = "https://www.pushplus.plus//send"
 
 pushplus_data = {
     "token": pushplus_token,
     "title": "每天60秒读懂世界",
-    "content": "{}<br/><img src='{}' />".format(content,image_url),
+    "content": "{}".format(c),
     # "topic": "群组ID",
     "template": "html"
 }
