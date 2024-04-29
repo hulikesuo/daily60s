d5133dbac8dfe5f46a703f2be422d15be47315e6
hulikesuo <167519414+hulikesuo@users.noreply.github.com>
2024-04-29 14:42:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
-import os
 import requests
 import json
-
-pushplus_token = os.environ.get('pushplus_token')
-topic = os.environ.get('topic')
-
-text_url = "https://60s.viki.moe/60s?v2=1"
-
-text_response = requests.get(text_url)
-content = text_response.text
-content = json.loads(content)
-s = content['data']['news']
-m = content['data']['tip']
-c = ''
-for i in range(len(s)):
-    c += f'{i+1}、{s[i]}\n'
-c += f'微语: {m}'
-
+url = 'https://api.jun.la/60s.php?format=imgapi'
+text = requests.get(url)
+content = json.loads(text.text)
+pushplus_token = '48abcdf31042406bb4dfd244df0bb34e'
+image_url = content['imageBaidu']
 pushplus_url = "https://www.pushplus.plus//send"
-
 pushplus_data = {
-    "token": pushplus_token,
-    "title": "每天60秒读懂世界",
-    "content": "{}".format(c),
-    # "topic": "群组ID",
-    "template": "html"
+"token": pushplus_token,
+"title": "每日60s新闻",
+"content": "<br/><img src='{}' alt=""/>".format(image_url),
+# "topic": "群组ID",
+"template": "html"
 }
 
 requests.post(pushplus_url, json=pushplus_data)
